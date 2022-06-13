0f6752fe322f361cc86bc86b58d830b964b7fe7f
Viktor Pinyagin <viktopiniaginya@gmail.com>
2022-06-13 20:50:26 +0500

    prepare appdelegate

--- work with tableview/ViewController.swift
+++ work with tableview/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+// MARK: - Views
+    
+// MARK: - Properties
+// MARK: - Settings
+// MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
